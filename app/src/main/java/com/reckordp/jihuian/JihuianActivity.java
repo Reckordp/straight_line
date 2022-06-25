2b164a0adfa33b7f706eea75183dda31f53bae96
Reckordp <rmuhammad134@gmail.com>
2022-06-25 08:48:10 +0700

    Initial Commit

---
+++ app/src/main/java/com/reckordp/jihuian/JihuianActivity.java
@@ -0,0 +1,14 @@
+package com.reckordp.jihuian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JihuianActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jihuian);
+    }
+}
