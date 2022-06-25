fddd66344c9e6c28f4d6ddfda5a0ef262ec14ef4
Reckordp <rmuhammad134@gmail.com>
2022-06-25 09:21:23 +0700

    Add login

--- app/src/main/java/com/reckordp/jihuian/JihuianActivity.java
+++ app/src/main/java/com/reckordp/jihuian/JihuianActivity.java
@@ -1,8 +1,10 @@
 package com.reckordp.jihuian;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class JihuianActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class JihuianActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jihuian);
+
+        String nama = tertulis(R.id.nama);
+        String pass = tertulis(R.id.pass);
+
+        findViewById(R.id.ok_login).setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("User tidak ditemukan");
+            builder.setMessage("Periksa Nama dan password, karena mungkin salah");
+            builder.setPositiveButton(android.R.string.ok, (i, ii) -> { });
+            builder.show();
+        });
+    }
+
+    private String tertulis(int id) {
+        return ((EditText)findViewById(id)).getText().toString();
     }
 }
