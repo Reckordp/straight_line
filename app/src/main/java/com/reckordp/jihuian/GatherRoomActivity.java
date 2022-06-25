b3f83571e7f6b4d2e8f33614a424f9ad677f0c81
Reckordp <rmuhammad134@gmail.com>
2022-06-25 10:29:51 +0700

    Add edit and kembali

--- app/src/main/java/com/reckordp/jihuian/GatherRoomActivity.java
+++ app/src/main/java/com/reckordp/jihuian/GatherRoomActivity.java
@@ -2,6 +2,7 @@ package com.reckordp.jihuian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class GatherRoomActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class GatherRoomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gather_room);
+
+        findViewById(R.id.edit).setOnClickListener(v -> {
+            Intent intent = new Intent(this, PKActivity.class);
+            intent.putExtra(PKActivity.MENGUBAH_PESAN, "");
+            intent.putExtra(PKActivity.MENGUBAH_KESAN, "");
+            startActivity(intent);
+            finish();
+        });
+
+        findViewById(R.id.kembali).setOnClickListener(v -> finish());
     }
 }
--- app/src/main/java/com/reckordp/jihuian/PKActivity.java
+++ app/src/main/java/com/reckordp/jihuian/PKActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class PKActivity extends AppCompatActivity {
+    public static String MENGUBAH_PESAN = "UBAH PESAN";
+    public static String MENGUBAH_KESAN = "UBAH KESAN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pkactivity);
+
+        EditText pesan = findViewById(R.id.pesan);
+        EditText kesan = findViewById(R.id.kesan);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(MENGUBAH_PESAN)) {
+            pesan.setText(intent.getStringExtra(MENGUBAH_PESAN));
+        }
+
+        if (intent.hasExtra(MENGUBAH_KESAN)) {
+            kesan.setText(intent.getStringExtra(MENGUBAH_KESAN));
+        }
 
         findViewById(R.id.kirim).setOnClickListener(v -> {
             startActivity(new Intent(this, GatherRoomActivity.class));
